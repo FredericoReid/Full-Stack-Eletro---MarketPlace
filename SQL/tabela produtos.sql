4e1774b63e8871612eda010eb175b6db321808e3
FredericoReid <fredericoreid@hotmail.com>
2020-10-25 23:21:21 -0300

    Criação da tabela Produtos SQL
    Comandos para a inserção dos produtos na tabela produtos

---
+++ SQL/tabela produtos.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM fseletro.produtos
+LIMIT 0, 1000
+
+-- Date: 2020-10-25 23:16
+*/
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (1,'geladeira','Geladeira Brastemp',899,899,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.03%20(2).jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (2,'geladeira','Geladeira tres portas',1500,1500,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.03.jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (3,'geladeira','Geladeira Dodge',999,999,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.04%20(1).jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (4,'fogao','Fogão 4 bocas',679.8,679.8,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.02%20(1).jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (5,'fogao','Fogao ATLAS',599.9,599.9,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.02.jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (6,'microondas','Geladeira Dodge',299.99,299.99,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.02%20(2).jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (7,'microondas','Microondas Eletrolux',490,490,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.04%20(3).jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (8,'microondas','Microondas Philco',1500,1500,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.06.jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (9,'maquina de lavar','Maquina de lavar',899.9,899.9,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.04%20(2).jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (10,'maquina de lavar','Maquina de lavar Philco',699.99,699.99,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.05.jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (11,'lava loucas','Lavadora de Pratos',599.9,599.9,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.04.jpeg');
+INSERT INTO `` (`id`,`categoria`,`descricao`,`preco`,`preco_venda`,`imagem`) VALUES (12,'lava loucas','Lavadora de Pratos Elec.',599.9,599.9,'imagens/WhatsApp%20Image%202020-09-24%20at%2018.08.05%20(1).jpeg');
