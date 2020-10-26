306474c40c2d153418da959bcae5c2b08c7c1cc9
FredericoReid <fredericoreid@hotmail.com>
2020-10-26 00:06:56 -0300

    Criação das tabelas Produto e Pedido
    Criação e inserção de dados nas tabelas Produto e Pedido

---
+++ SQL/Tabela Pedidos pronta.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM fseletro.pedidos
+LIMIT 0, 1000
+
+-- Date: 2020-10-26 00:05
+*/
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (1,'Marcos Rocha','Ruas das Palmeiras, 718 - Vila Santa Clementina','99999-9999','Geladeira Três Portas',1500,2,3000);
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (2,'Fernanda Silva','Rua Rua Deolinda de Almeida Corrêa , 13 - Distrito Industrial Nova Avaré','99999-9999','Fogão ATLAS',599.9,1,599.9);
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (3,'Paulo Santos','Rua Adutora, 590 - Chácara São Domingos Shop','99999-9999','Geladeira Dodge',1500,2,3000);
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (4,'Sara Carolina','Rua Silvio de Campos Gurgel, 395 - Residencial Ouro Verde','99999-9999','Geladeira Brastemp',899.9,1,899.9);
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (5,'Simone Martins','Alameda dos Beija-Flor, 349 - Recanto Campo Belo','99999-9999','Lavadora de Pratos Elec.',599.9,3,1799.7);
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (6,'Raul Vitor','Rua Anhanguera, 952 - Jardim do Prado','99999-9999','Máquina de Lavar',899.9,5,4499.5);
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (7,'Mirella Clara','Rua Augusto Avelino de Morais, 752 - Parque das Cerejeiras','99999-9999','Microondas Philco',490,2,980);
+INSERT INTO `` (`id`,`nome_cliente`,`endereco`,`telefone`,`nome_produto`,`valor_unitario`,`quantidade`,`valor_total`) VALUES (8,'Bryan Filipe','Rua Luiz Borges, 604 - Jardim Veneto II','99999-9999','Geladeira Dodge',999.9,1,999.9);
