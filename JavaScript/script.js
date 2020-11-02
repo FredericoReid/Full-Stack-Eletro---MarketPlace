70a6e43c79612464bf304df8e4fb201d1795a25d
FredericoReid <fredericoreid@hotmail.com>
2020-11-02 19:57:34 -0300

    Reparação das funções de zoom, procura e exibir categoria

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,31 +1,31 @@
-function Procura() {
+function Procura('Procurainicial') {
 
-    var produtos = ["geladeira", "microondas", "fogao", "lavadora", "maquina de lavar"];
+    let produtos = ["geladeira", "microondas", "fogao", "lavadora", "maquina de lavar"];
 
-    var procurar1 = document.getElementById("Procurainicial");
+    let procurar1 = document.getElementById("Procurainicial");
 
     for (let i = 0; i < produtos.length; i++) {
       if (produtos[i] == procurar1) {
-        var auxiliar = 1;
+        let auxiliar = 1;
       }
     }
 
-    if (auxiliar > 0) {
+    if (auxiliar == 1) {
       alert('Achamos seu produto! Dê uma olhada abaixo nessa mesma página');
     }
     
     else {
       alert('Não achamos seu produto!  :( ');
     }
-
     return 0;
+
 }
 
 function exibirCateg(categoria) {
   let elementos = document.getElementsByClassName("produtos");
   console.log(elementos[0].id);
   console.log(categoria);
-  for (var i = 0; i < elementos.length; i++) {
+  for (let i = 0; i < elementos.length; i++) {
       console.log(elementos[i].id);
       if (categoria == elementos[i].id) {
           elementos[i].style = "display:inline-block";
@@ -36,7 +36,7 @@ function exibirCateg(categoria) {
   }
 }
 
-function destaque = (imagem) => {
+let destaque = (imagem) => {
   imagem.width == 240 ? imagem.width = 140 : imagem.width = 240
 }
 
